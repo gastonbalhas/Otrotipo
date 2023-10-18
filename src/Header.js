0f66a0a646a32b2174556812e476bef515c4a811
gastonbalhas <gastonbalhas@gmail.com>
2023-10-18 10:59:26 -0300

    fotos webp

--- src/Header.js
+++ src/Header.js
@@ -5,18 +5,18 @@ import Navbar from './Navbar';
 function Header() {
   return (
     <div>
-      <div className="d-lg-block d-md-none"> {/* Oculta Navbar en pantallas medianas y pequeñas */}
+      <div className="d-lg-block d-md-none">
         <Navbar />
       </div>
       <Carousel>
         <Carousel.Item>
-          <img src="./img/renzo1.jpg" className="d-block w-100" alt="Imagen 1" />
+          <img src="./img/renzo1.webp" className="d-block w-100" alt="Imagen 1" />
         </Carousel.Item>
         <Carousel.Item>
-          <img src="./img/mono1.jpg" className="d-block w-100" alt="Imagen 2" />
+          <img src="./img/mono1.webp" className="d-block w-100" alt="Imagen 2" />
         </Carousel.Item>
         <Carousel.Item>
-          <img src="./img/vivoGral2.jpg" className="d-block w-100" alt="Imagen 3" />
+          <img src="./img/vivoGral2.webp" className="d-block w-100" alt="Imagen 3" />
         </Carousel.Item>
       </Carousel>
       <section className="text-light">
